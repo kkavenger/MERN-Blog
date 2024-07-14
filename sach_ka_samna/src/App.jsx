9349f096db77621eb98d6e9e6fae6b8eaf537da2
Kunal Mishra <avenger1.tonystark@gmail.com>
2024-07-14 23:12:51 +0530

    resolving router error

--- sach_ka_samna/src/App.jsx
+++ sach_ka_samna/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
+import Dashboard from './pages/dashboard'
+import Project from './pages/project'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/project' element={<Project/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- sach_ka_samna/src/pages/dashboard.jsx
+++ sach_ka_samna/src/pages/dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function dashboard() {
+export default function Dashboard() {
   return (
     <div>
-      dashboard
+      Dashboard
     </div>
   )
 }
